fa61f0bf76b2c49d8101c4aa2e8edaf99bb6e871
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-07-15 10:01:34 -0500

    se creo la matriz de tamanio n

---
+++ matrices1.cpp
@@ -0,0 +1,9 @@
+//halle la sumatoria de elementos 
+#include <iostream>
+using namespace std;
+int main (){
+    int n;
+    cout << "Ingrese el tamanio de la matriz cuadrada: "; cin >> n;
+    int matriz[n][n];
+
+}
