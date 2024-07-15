7eda98529224aadf7b9e70139a8991677360cb7c
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-07-15 10:36:20 -0500

    se anadio la estructura for para visualizar la matriz

--- matrices1.cpp
+++ matrices1.cpp
@@ -1,4 +1,4 @@
-//halle la sumatoria de elementos 
+//halle la sumatoria de elementos de una matriz cuadrada encima de la diagonal principal
 #include <iostream>
 using namespace std;
 
@@ -15,9 +15,15 @@ int suma (int n, int matriz[][100]){
 }
 
 int main (){
-    int n, sumatoria;
+    int n, sumatoria, matriz[n][n];
     cout << "Ingrese el tamanio de la matriz cuadrada: "; cin >> n;
-    int matriz[n][n];
+    cout << "La matriz es de elementos: " << endl;
+    for (int i = 0 ; i < n; i++){
+        for (int j = 0; i + 1 < n; j++){
+            cout << matriz[i][j];
+        }
+        cout << endl;
+    }
     sumatoria = suma(n, matriz);
     cout << "La sumatoria de la matriz es: " << sumatoria;
     return 0;
