c14d87947571d663cc17461eaacdce0affe9cef5
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-07-15 10:21:15 -0500

    se termino la funcion suma y se complemento el programa principal

--- matrices1.cpp
+++ matrices1.cpp
@@ -4,18 +4,21 @@ using namespace std;
 
 int suma (int n, int matriz[][100]);
 
-int suma (int n, int matriz[][100])
+int suma (int n, int matriz[][100]){
     int sumatoria = 0;
     for (int i = 0 ; i < n; i++){
-        for (int j = 0; j < n; j++){
-            
+        for (int j = 0; i + 1 < n; j++){
+            sumatoria += matriz[][];
         }
-        
     }
+    return sumatoria;
+}
 
 int main (){
-    int n;
+    int n, sumatoria;
     cout << "Ingrese el tamanio de la matriz cuadrada: "; cin >> n;
     int matriz[n][n];
-
+    sumatoria = suma(n, matriz);
+    cout << "La sumatoria de la matriz es: " << sumatoria;
+    return 0;
 }
