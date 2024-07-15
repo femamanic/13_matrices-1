171a322060b7724e1abfca061249aa43cbc7e504
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-07-15 10:14:57 -0500

    se anadio el gitignore y la funcion suma

--- matrices1.cpp
+++ matrices1.cpp
@@ -1,6 +1,18 @@
 //halle la sumatoria de elementos 
 #include <iostream>
 using namespace std;
+
+int suma (int n, int matriz[][100]);
+
+int suma (int n, int matriz[][100])
+    int sumatoria = 0;
+    for (int i = 0 ; i < n; i++){
+        for (int j = 0; j < n; j++){
+            
+        }
+        
+    }
+
 int main (){
     int n;
     cout << "Ingrese el tamanio de la matriz cuadrada: "; cin >> n;
