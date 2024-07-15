3b700f4b2fe31bab2de851e24f48f8bf8fa8845c
dbmamanir <dbmamanir@unjbg.edu.pe>
2024-07-15 10:57:30 -0500

    se corrigio errores

--- matrices1.cpp
+++ matrices1.cpp
@@ -2,39 +2,42 @@
 #include <iostream>
 using namespace std;
 
-int suma (int n, int matriz[][100]);
+int suma(int n, int matriz[][100]);
 
-int suma (int n, int matriz[][100]){
+int suma(int n, int matriz[][100]) {
     int sumatoria = 0;
-    for (int i = 0 ; i < n; i++){
-        for (int j = 0; i + 1 < n; j++){
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) { 
             sumatoria += matriz[i][j];
         }
     }
     return sumatoria;
 }
 
-int main (){
+int main() {
     int n, sumatoria;
-    cout << "Ingrese el tamanio de la matriz cuadrada: "; cin >> n;
+    cout << "Ingrese el tamaño de la matriz cuadrada: ";
+    cin >> n;
+    
     int matriz[n][100];
-
-
+    
     cout << "Ingrese los elementos de la matriz:" << endl;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++) {
             cin >> matriz[i][j];
         }
     }
-
-    cout << "La matriz es: " << endl;
-    for (int i = 0 ; i < n; i++){
-        for (int j = i + 1; j < n; j++){
-            cout << matriz[i][j];
+    
+    cout << "La matriz es:" << endl;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            cout << matriz[i][j] << " ";
         }
         cout << endl;
     }
+    
     sumatoria = suma(n, matriz);
-    cout << "La sumatoria de la matriz por encima de la diagonal principal es: " << sumatoria;
+    cout << "La sumatoria de los elementos encima de la diagonal principal es: " << sumatoria << endl;
+
     return 0;
 }
